d802de12ab268f0af362c3cb658a501bd818a9ce
Yves Talboom <yves.talboom@gmail.com>
2014-04-19 09:46:47 +0200

    Setup test for rectangle collision detection

--- PongGame/main.cpp
+++ PongGame/main.cpp
@@ -4,11 +4,20 @@
 // Here is a small helper for you ! Have a look.
 #include "ResourcePath.hpp"
 
+bool doRectanglesIntersect(const sf::RectangleShape &rectangle1, sf::RectangleShape &rectangle2);
+
+void setUpBorderRectangle(sf::RectangleShape &topRectangle);
+
+void setUpBall(int const mainWindowWidth, int const mainWindowHeight, sf::RectangleShape &ball);
+
 int main(int, char const **) {
     using namespace sf;
 
+    const int mainWindowWidth = 640;
+    const int mainWindowHeight = 480;
+
     // Create the main window
-    RenderWindow window(VideoMode(800, 600), "SFML window");
+    RenderWindow window(VideoMode(mainWindowWidth, mainWindowHeight), "Pong");
 
     // Set the Icon
     Image icon;
@@ -22,6 +31,37 @@ int main(int, char const **) {
     if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
         return EXIT_FAILURE;
     }
+
+    // setup rectangles
+    RectangleShape topRectangle;
+    RectangleShape leftRectangle;
+    RectangleShape rightRectangle;
+    RectangleShape bottomRectangle;
+
+    const int borderSize = 30;
+
+    topRectangle.setPosition(borderSize, 0);
+    topRectangle.setSize(sf::Vector2f(mainWindowWidth - 2 * borderSize, borderSize));
+
+    leftRectangle.setPosition(0, 0);
+    leftRectangle.setSize(sf::Vector2f(borderSize, mainWindowHeight));
+
+    rightRectangle.setPosition(mainWindowWidth - borderSize, 0);
+    rightRectangle.setSize(sf::Vector2f(borderSize, mainWindowHeight));
+
+    bottomRectangle.setPosition(borderSize, mainWindowHeight - borderSize);
+    bottomRectangle.setSize(sf::Vector2f(mainWindowWidth - 2 * borderSize, borderSize));
+
+    setUpBorderRectangle(topRectangle);
+    setUpBorderRectangle(leftRectangle);
+    setUpBorderRectangle(rightRectangle);
+    setUpBorderRectangle(bottomRectangle);
+
+    // setup ball
+    RectangleShape ball;
+    setUpBall(mainWindowWidth, mainWindowHeight, ball);
+
+    Vector2f ballSpeed(0.1, 0.1);
 
     // Start the game loop
     while (window.isOpen()) {
@@ -39,12 +79,49 @@ int main(int, char const **) {
             }
         }
 
+        if (doRectanglesIntersect(ball, topRectangle) || doRectanglesIntersect(ball, bottomRectangle)) {
+            ballSpeed.y = -ballSpeed.y;
+        }
+
+        if (doRectanglesIntersect(ball, leftRectangle) || doRectanglesIntersect(ball, rightRectangle)) {
+            ballSpeed.x = -ballSpeed.x;
+        }
+
+        ball.move(ballSpeed.x, ballSpeed.y);
+
         // Clear screen
-        window.clear();
+        window.clear(Color::White);
+        window.draw(topRectangle);
+        window.draw(leftRectangle);
+        window.draw(rightRectangle);
+        window.draw(bottomRectangle);
+        window.draw(ball);
 
         // Update the window
         window.display();
     }
 
     return EXIT_SUCCESS;
+}
+
+// test if two given rectangles intersect
+bool doRectanglesIntersect(const sf::RectangleShape &rectangle1, sf::RectangleShape &rectangle2) {
+    sf::FloatRect globalBoundsOfRectangle1 = rectangle1.getGlobalBounds();
+    sf::FloatRect globalBoundsOfRectangle2 = rectangle2.getGlobalBounds();
+    return globalBoundsOfRectangle1.intersects(globalBoundsOfRectangle2);
+}
+
+void setUpBorderRectangle(sf::RectangleShape &topRectangle) {
+    const int outlineThickness = 3;
+    topRectangle.setFillColor(sf::Color(100, 100, 100));
+    topRectangle.setOutlineColor(sf::Color::Blue);
+    topRectangle.setOutlineThickness(outlineThickness);
+}
+
+void setUpBall(int const mainWindowWidth, int const mainWindowHeight, sf::RectangleShape &ball) {
+    ball.setPosition(mainWindowWidth / 2, mainWindowHeight / 2);
+    ball.setSize(sf::Vector2f(20, 20));
+    ball.setFillColor(sf::Color::Red);
+    ball.setOutlineColor(sf::Color::Yellow);
+    ball.setOutlineThickness(1);
 }
